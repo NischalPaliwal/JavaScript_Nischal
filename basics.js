b5a339ffc26e0993fb3bab9c49390e3eefe9e56a
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-03-25 18:04:27 +0000

    edit

--- basics.js
+++ basics.js
@@ -8,3 +8,22 @@ console.log(accountId)
 console.log(accountMail)
 console.table([accountId,accountMail,accountPassword,accountCity])
 "use strict" // treat all JS code as newer version
+
+// Data Types and their Conversion ->
+let score1 = 33
+let score2 = "33abc"
+let valueInNumber = Number(score2)
+console.table([typeof(score1),typeof(score2),typeof(valueInNumber),valueInNumber])
+
+score2 = null
+valueInNumber = Number(score2)
+console.table([typeof(score2),typeof(valueInNumber),valueInNumber])
+
+score2 = undefined
+valueInNumber = Number(score2)
+console.table([typeof(score2),typeof(valueInNumber),valueInNumber])
+
+score2 = true
+valueInNumber = Number(score2)
+console.table([typeof(score2),typeof(valueInNumber),valueInNumber])
+
