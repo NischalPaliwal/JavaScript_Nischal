89743f75966ea0d6ef1d2003fffef92480f89a01
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-03-25 17:51:10 +0000

    edit

---
+++ basics.js
@@ -0,0 +1,10 @@
+// Assigning Values ->
+const accountId = 45
+let accountMail = "nischal@gmail.com"
+let accountPassword = "23432"
+let accountCity = "Jaipur"
+// Printing Values ->
+console.log(accountId)
+console.log(accountMail)
+console.table([accountId,accountMail,accountPassword,accountCity])
+"use strict" // treat all JS code as newer version
