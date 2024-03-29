c454fcd9c3dee4f4104b116d74aacdcc4b4bc88a
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-03-29 13:00:25 +0000

    edit

--- basics.js
+++ basics.js
@@ -54,3 +54,7 @@ console.log("1" + 2);
 console.log(1 + "2");
 console.log("1" + 2 + 2);
 console.log(2 + 2 + "1");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);  // Increment Operator
