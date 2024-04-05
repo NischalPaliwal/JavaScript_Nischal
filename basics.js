f6a144a97e520cd83bb0a52d385caba165c8c0a7
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-04-05 17:36:50 +0000

    edit

--- basics.js
+++ basics.js
@@ -123,3 +123,8 @@ console.log(s2.charAt(4));
 console.log(s2.indexOf('r'));
 console.log(s2.substring(0,6));  // character at second index in the substring function is not printed.
 console.log(s2.slice(0,6));  // slice function also obeys negative values.
+
+let s4 = "     hello     "
+console.log(s4);
+console.log(s4.trim());  // removes front and back spaces
+console.log(s4.replace())
