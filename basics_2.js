62da86f40d5658384b76309266cbdcd063fdccf1
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-04-10 03:26:58 +0000

    commit

--- basics_2.js
+++ basics_2.js
@@ -1,14 +1,19 @@
 const score = 400
-console.log(score);
+//console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+//console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toString().length);
+//console.log(balance.toString());
+//console.log(balance.toString().length);
 
-console.log(balance.toFixed(2));   // Imp.
+//console.log(balance.toFixed(2));   // Imp.
 
 const otherNumber = 234.8567
-console.log(otherNumber.toPrecision(3));
-console.log(otherNumber.toPrecision(4));
+//console.log(otherNumber.toPrecision(3));  // Imp.
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));   // 10,00,000
+
+// Maths in JavaScript ->
