fbea0ce170165a3cc00696a4cc858c73912b5ac9
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-04-10 03:33:53 +0000

    commit

--- basics_2.js
+++ basics_2.js
@@ -22,3 +22,5 @@ console.log(Math.abs(-5));
 console.log(Math.round(5.768));
 console.log(Math.ceil(5.768));
 console.log(Math.floor(5.768));
+console.log(Math.min(4, 5, 6, 7, 8));
+console.log(Math.max(4, 5, 6, 7, 8));
