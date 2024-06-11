8d6b31f693fc48707a850c339b49883d1b8b689d
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-06-11 07:50:41 +0000

    of

--- basics_2.js
+++ basics_2.js
@@ -91,3 +91,16 @@ const dc_heroes = ["superman", "flash", "batman"]
 console.log(marvel_heroes.concat(dc_heroes));
 const all_heroes = [...marvel_heroes, ...dc_heroes]
 console.log(all_heroes);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7,[9,7]]]
+const usable_another_array = another_array.flat(Infinity)
+console.log(usable_another_array);
+
+console.log(Array.isArray("Nischal"));
+console.log(Array.from("Nischal"));
+console.log(Array.from({name: "nischal"}));   //imp.
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1, score2, score3));
