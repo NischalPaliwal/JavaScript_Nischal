075f80cbc1abff4eb3e587730b28fd3a7bc1e0ab
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-06-19 11:57:33 +0000

    promises

--- file_read.js
+++ file_read.js
@@ -3,3 +3,18 @@ const fs = require("fs");
 fs.readFile("a.txt", "utf-8", function(err, data) {
     console.log(data);
 })
+
+function nischalReadFile() {
+    return new Promise( function(resolve) {
+     fs.readFile("a.txt", "utf-8", function(err, data) {
+        resolve(data);
+     });
+    })
+}
+
+// callback function to call
+function onDone(data) {
+    console.log(data);
+}
+
+nischalReadFile().then(onDone);
